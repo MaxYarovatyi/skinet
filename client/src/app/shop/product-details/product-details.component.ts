8d7b5953c947207e7d54717d734d5109fdb299c6
Max Yarovatyi <yarovatyimax@gmail.com>
2023-02-28 19:11:28 +0200

    Added interceptors, styles, error handlings and some other features

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/shared/models/product';
 import { ShopService } from '../shop.service';
-
+import { BreadcrumbService } from 'xng-breadcrumb';
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
@@ -12,8 +12,11 @@ export class ProductDetailsComponent implements OnInit {
   product: Product;
   constructor(
     private shopService: ShopService,
-    private activatedRoute: ActivatedRoute
-  ) {}
+    private activatedRoute: ActivatedRoute,
+    private bcService: BreadcrumbService
+  ) {
+    this.bcService.set('@productDetails', ' ');
+  }
 
   ngOnInit(): void {
     this.loadProduct();
@@ -24,7 +27,7 @@ export class ProductDetailsComponent implements OnInit {
       .subscribe(
         (product) => {
           this.product = product;
-          console.log(product);
+          this.bcService.set('@productDetails', product.name);
         },
         (error) => {
           console.log(error);
