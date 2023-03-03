831c9cf25e9a174371c9f4b02eab1730215e1cd9
Max Yarovatyi <yarovatyimax@gmail.com>
2023-03-03 17:44:24 +0200

    Basket component and basket functionality added

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { BasketService } from './basket/basket.service';
 import { Pagination } from './shared/models/pagination';
 import { Product } from './shared/models/product';
 
@@ -12,7 +13,17 @@ export class AppComponent implements OnInit {
   title = 'Skinet';
   products: Product[];
 
-  constructor() {}
+  constructor(private basketService: BasketService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const basketId = localStorage.getItem('basket_id');
+    if (basketId) {
+      this.basketService.getBasket(basketId).subscribe(
+        () => console.log('initialized basket'),
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
+  }
 }
